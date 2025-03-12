c55b689354cd9c595cc0c68322f57194184224f2
Imran <imranbinazad777@gmail.com>
2025-03-12 19:11:48 +0600

    upadated

--- backend/index.js
+++ backend/index.js
@@ -4,15 +4,14 @@ const mongoose = require("mongoose")
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
-app.use(cors)
+app.use(cors())
 app.use(express.json())
 
-app.get('/',(req,res)=>{
-    return res.send("welcome to home route of onestop backend")
+app.get("/server",(req,res)=>{
+    console.log("request arrived at endpoint")
+    res.send("welcome  home route of onestop backend");
 })
 
-app.listen(PORT,()=>{
-    console.log(`Server is running in ${PORT}`);
-})
+app.listen(PORT,()=>console.log(`Server is running in localhost:${PORT}`))
